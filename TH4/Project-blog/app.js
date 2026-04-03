084a47f1e12d9a8883223b2901fa3e09067c672d
Gia Hung <giahung.ex21@gmail.com>
2026-04-03 13:19:23 +0700

    Hoàn tất thêm bài thực hành TH4 và Project-blog

---
+++ TH4/Project-blog/app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const BlogPost = require('./models/BlogPost');
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+
+app.set('view engine', 'ejs');
+
+// Kết nối MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB')
+  .then(() => console.log('Kết nối MongoDB thành công'))
+  .catch(err => console.log(err));
+
+// ===== ROUTES =====
+
+// Trang chủ (sắp xếp bài mới nhất lên trên)
+app.get('/', async (req, res) => {
+  const posts = await BlogPost.find({}).sort({ createdAt: -1 });
+  res.render('index', { posts });
+});
+
+// Form tạo bài
+app.get('/blogposts/new', (req, res) => {
+  res.render('create');
+});
+
+// Lưu bài viết
+app.post('/blogposts/store', async (req, res) => {
+  await BlogPost.create({
+    title: req.body.title,
+    body: req.body.body
+  });
+  res.redirect('/');
+});
+
+// Xem chi tiết
+app.get('/blogposts/:id', async (req, res) => {
+  const post = await BlogPost.findById(req.params.id);
+  res.render('detail', { post });
+});
+
+// Form sửa
+app.get('/blogposts/edit/:id', async (req, res) => {
+  const post = await BlogPost.findById(req.params.id);
+  res.render('edit', { post });
+});
+
+// Cập nhật
+app.put('/blogposts/update/:id', async (req, res) => {
+  await BlogPost.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    body: req.body.body
+  });
+  res.redirect('/');
+});
+
+// Xóa
+app.delete('/blogposts/:id', async (req, res) => {
+  await BlogPost.findByIdAndDelete(req.params.id);
+  res.redirect('/');
+});
+
+// Server chạy
+app.listen(3000, () => {
+  console.log('Server chạy tại http://localhost:3000');
+});
